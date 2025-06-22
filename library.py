0731169262814ff5615bddb826fb9ea0c2144a9c
sidpatil10 <141855037+sidpatil10@users.noreply.github.com>
2025-06-22 22:28:13 +0530

    Add files via upload

---
+++ library.py
@@ -0,0 +1,9 @@
+pip install streamlit transformers sentencepiece
+
+pip install streamlit requests
+
+! pip install streamlit -q
+
+!wget -q -O - ipv4.icanhazip.com
+
+! streamlit run app.py & npx localtunnel --port 8501
